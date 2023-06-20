8049261c432b7fbeecf7f053d99f4a5d34679e03
Zero-definition <70338414+Zero-definition@users.noreply.github.com>
2023-06-21 00:36:13 +0530

    Time: 9 ms (5.08%), Space: 44.4 MB (5.62%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        String c="";
+        int l=s.length();
+        for(int i=0;i<l;i++)
+        {
+            if(c==""||s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{')
+            {
+                c=s.charAt(i)+c;
+            }
+            else if(s.charAt(i)==')')
+            {
+                if(c.charAt(0)=='(')
+                c=c.substring(1,c.length());
+                else
+                c=s.charAt(i)+c;
+            }
+            else if(s.charAt(i)=='}')
+            {
+                if(c.charAt(0)=='{')
+                c=c.substring(1,c.length());
+                else
+                c=s.charAt(i)+c;
+            }
+            else if(s.charAt(i)==']')
+            {
+                if(c.charAt(0)=='[')
+                c=c.substring(1,c.length());
+                else
+                c=s.charAt(i)+c;
+            }
+        }
+        if(c=="")
+        return(true);
+        else 
+        return(false);
+    }
+}
